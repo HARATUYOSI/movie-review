6c6fde8c7544613282000f03722966f7953b87a5
HARATUYOSI <alra9si916323@gmail.com>
2018-07-08 12:01:18 +0000

    登録編集機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def index
   end
+  def show
+    @user = User.find(params[:id])
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "ユーザ情報を編集しました"
+      redirect_to user_path(@user.id)
+    else
+       render root_path
+    end
+  end
+  private
+  def user_params
+      params.require(:user).permit(:name, :introduction, :email, :image)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_07_141437) do
+ActiveRecord::Schema.define(version: 2018_07_08_094621) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,6 +42,11 @@ ActiveRecord::Schema.define(version: 2018_07_07_141437) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "introduction"
+    t.text "image_id"
+    t.integer "follower_count", default: 0
+    t.integer "follwing_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
